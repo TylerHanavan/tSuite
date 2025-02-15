9f2489e45bd9ad0ca2a59ea38a031d609e2c85c4
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-15 14:36:12 -0500

    db connection & debug

--- www/index.php
+++ www/index.php
@@ -2,4 +2,14 @@
 
     include 'sql_helper.php';
 
+    $tsuite_conn = get_database_connection('localhost', 'tsuite_admin', 'password', 'tsuite');
+
+    $request_method = $_SERVER['REQUEST_METHOD']; // GET, POST, PUT, DELETE...
+    $request_uri = rtrim($_SERVER['REQUEST_URI'], '/'); // /example?foo=bar1&foo=bar2 => /example?foo=bar2
+    $request_is_https = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') == true; // false
+
+    echo "Request Method: $request_method\n";
+    echo "Request URI: $request_uri\n";
+    echo "Request is HTTPS: $request_is_https\n";
+
 ?>
