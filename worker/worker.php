cc048af9082f8219ed4f291fc193fbec7a370906
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-15 12:21:53 -0500

    Initial commit

---
+++ worker/worker.php
@@ -0,0 +1,51 @@
+<?php
+
+    $tsuite_config_location = get_tsuite_config_location();
+
+    $_TSUITE_CONFIG = get_tsuite_config($tsuite_config_location);
+
+    $repo = get_config_value('REPO');
+    $repo_url = get_config_value('REPO_URL');
+    $branch = get_config_value('BRANCH');
+
+    function get_tsuite_config($tsuite_config_location) {
+        $ret_conf = array();
+        $line = strtok($config_file_contents, "\r\n");
+
+        while($line != false) {
+    
+            $kv = explode('=', $line);
+    
+            if($retrieved_value != false) {
+                $ret_conf[$kv[0]] = $retrieved_value;
+            } else {
+                $ret_conf[$kv[0]] = $kv[1];
+            }
+    
+            $line = strtok("\r\n");
+        }
+
+        return $ret_conf;
+    }
+
+    function get_config_value($key) {
+        global $_TSUITE_CONFIG;
+
+        return (isset($_TSUITE_CONFIG[$key]) && !empty($_TSUITE_CONFIG[$key])) ? $_TSUITE_CONFIG[$key] : null;
+    }
+
+    function get_tsuite_config_location() {
+        return getenv('TSUITE_CONFIG_LOCATION');
+    }
+
+    function read_flat_file($path) {
+        $ret = '';
+        $file = fopen($path, "r") or die("Unable to open file: $path");
+        while(($read = fgets($file)) != null) {
+            $ret .= $read;
+        }
+        fclose($file);
+        return $ret;
+    }
+
+?>
