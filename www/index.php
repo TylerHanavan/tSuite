edc5b2cad1758544e5c07bb270ecf13d833413e9
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-15 14:42:50 -0500

    uri and uri arguments parsing

--- www/index.php
+++ www/index.php
@@ -5,11 +5,18 @@
     $tsuite_conn = get_database_connection('localhost', 'tsuite_admin', 'password', 'tsuite');
 
     $request_method = $_SERVER['REQUEST_METHOD']; // GET, POST, PUT, DELETE...
-    $request_uri = rtrim($_SERVER['REQUEST_URI'], '/'); // /example?foo=bar1&foo=bar2 => /example?foo=bar2
+    $request_uri = '/' . ltrim(rtrim($_SERVER['REQUEST_URI'], '/'), '/'); // /example?foo=bar1&foo=bar2 => /example?foo=bar2
     $request_is_https = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') == true; // false
 
     echo "Request Method: $request_method<br />";
-    echo "Request URI: $request_uri<br />";
     echo "Request is HTTPS: $request_is_https<br />";
+    echo "Request URI: $request_uri<br />";
+
+    $uri_path = explode('?', $request_uri)[0];
+    $uri_query = explode('?', $request_uri)[1];
+
+    echo "URI Path: $uri_path<br />";
+    echo "URI Query: $uri_query<br />";
+
 
 ?>
