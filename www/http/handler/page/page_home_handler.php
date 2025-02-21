f85e856fb8d4cce41f6729165a897cb053d88b81
Tyler Hanavan <TylerHanavan@gmail.com>
2025-02-20 22:06:48 -0500

    page handler for /

---
+++ www/http/handler/page/page_home_handler.php
@@ -0,0 +1,25 @@
+<?php
+
+    function handle_page_home_get($uri_parts, $uri_args) {
+
+        $repos = query('SELECT * FROM repo');
+        echo '<table><tr><th>Name</th><th>URL</th><th>Download Location</th><th>Install Location</th></tr>';
+        foreach($repos as $repo) {
+            $name = $repo['name'];
+            $url = $repo['url'];
+            $download_location = $repo['download_location'];
+            $install_location = $repo['install_location'];
+
+            echo "<tr><td><a href='/repo/$name'>$name</a></td><td><a href='$url'>$url</a></td><td>$download_location</td><td>$install_location</td></tr>";
+        }
+        echo '</table>';
+
+        if(is_worker_running()) {
+            echo '<br /><strong>Worker is running</strong>';
+        } else {
+            echo '<br /><strong>Worker is not running</strong>';
+        }
+
+    }
+
+?>
