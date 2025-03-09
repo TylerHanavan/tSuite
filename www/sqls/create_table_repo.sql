f7c9182df91276a63a51343d508a405377d8a48a
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-09 10:28:17 -0400

    remove install location from repo table def

--- www/sqls/create_table_repo.sql
+++ www/sqls/create_table_repo.sql
@@ -2,6 +2,5 @@ CREATE TABLE repo (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     url VARCHAR(300) NOT NULL,
-    download_location VARCHAR(300) NOT NULL,
-    install_location VARCHAR(300) NOT NULL
+    download_location VARCHAR(300) NOT NULL
 );
