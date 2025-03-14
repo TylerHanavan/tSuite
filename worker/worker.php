25ad00ae2449558b978345d89bd7a397b874053c
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-14 08:26:53 -0400

    update path to worker file

--- worker/worker.php
+++ worker/worker.php
@@ -2,9 +2,11 @@
 
     $tick = 0;
 
+    echo $path = dirname(__FILE__);
+
     while(true) {
         if($tick++ % 10 == 0) {
-            echo shell_exec('php test_worker.php');
+            echo shell_exec("php $path/test_worker.php");
         }
         sleep(1);
     }
