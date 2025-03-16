19b24f33945b49979a0b29809bd85c9e04e673c8
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-16 12:05:51 -0400

    modify setup file

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 # First, ensure you have Composer installed
 sudo apt update
 sudo apt install php-dom
-apt install composer
+sudo apt-get install php-xml
+sudo apt-get install php-mbstring
+sudo apt-get install php-zip unzip
 
-# Install PHPUnit globally
-composer global require phpunit/phpunit
+# Need to install composer in /usr/local/bin/composer
+# chmod +x /usr/local/bin/composer
 
-# Add Composer's global bin to your PATH (add this to ~/.bashrc)
-echo 'export PATH="$PATH:$HOME/.config/composer/vendor/bin"' >> ~/.bashrc
-source ~/.bashrc
+# SELENIUM
+#
+# Need to install chromedriver and place it somewhere
+# Need to install selenium-server and place it somewhere
+#
+# Need to install google-chrome-stable to test with Chrome
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo apt install ./google-chrome-stable_current_amd64.deb -y
+#
+# Launch Selenium with chromedriver
+# java -Dwebdriver.chrome.driver=/home/tyler/local_tsuite_code/chromedriver -jar /home/tyler/local_tsuite_code/selenium-server-4.29.0.jar standalone
