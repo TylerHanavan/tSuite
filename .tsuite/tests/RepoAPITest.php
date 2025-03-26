aa473969c748a0f54949a5cb1d021279d1edceed
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-26 07:25:34 -0400

    update actual to be string instead of array

--- .tsuite/tests/RepoAPITest.php
+++ .tsuite/tests/RepoAPITest.php
@@ -10,31 +10,31 @@
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", [], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"No entity provided in payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"No entity provided in payload"}', "$uri did not trigger error for empty payload");
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['name' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"Required field `url` is not provided in the payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Required field `url` is not provided in the payload"}', "$uri did not trigger error for empty payload");
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['url' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"Required field `name` is not provided in the payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Required field `name` is not provided in the payload"}', "$uri did not trigger error for empty payload");
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['download_location' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"Required field `name` is not provided in the payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Required field `name` is not provided in the payload"}', "$uri did not trigger error for empty payload");
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['name' => 'test', 'url' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"Required field `download_location` is not provided in the payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Required field `download_location` is not provided in the payload"}', "$uri did not trigger error for empty payload");
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['download_location' => 'test', 'url' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"Required field `name` is not provided in the payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Required field `name` is not provided in the payload"}', "$uri did not trigger error for empty payload");
 
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['name' => 'test', 'download_location' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals(json_decode($response['response'], true), '{"status":"failed","error":"Required field `url` is not provided in the payload"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Required field `url` is not provided in the payload"}', "$uri did not trigger error for empty payload");
 
         echo "Concluded testing POST $uri (errored second run)\n";
     }
