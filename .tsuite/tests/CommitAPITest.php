aef3edc1bdece5a609a62e899ec7dacd3277e287
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 06:44:15 -0400

    more tests with empty payloads

--- .tsuite/tests/CommitAPITest.php
+++ .tsuite/tests/CommitAPITest.php
@@ -8,12 +8,20 @@
     function test_commit_post_1($properties) {
         $uri = "/api/v1/commit";
 
-        $entities = array();
+        $entities = [];
 
         // Test missing payload
         $response = test_curl($properties['endpoint_url'] . "/$uri", $entities, true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals($response['response'], '{"status":"failed","error":"Resources array exists but is empty"}', "$uri did not trigger error for empty payload");
+        assertEquals($response['response'], '{"status":"failed","error":"Resources null or missing from payload"}', "$uri did not trigger error for empty payload");
+
+        $entity1 = [];
+        $entities[] = $entity1;
+
+        // Test missing payload
+        $response = test_curl($properties['endpoint_url'] . "/$uri", $entities, true);
+        assertEquals(400, $response['http_code'], "$uri http code mismatch");
+        assertEquals($response['response'], '{"status":"failed","error":"Resources null or missing from payload"}', "$uri did not trigger error for empty payload");
 
         echo "Concluded testing POST $uri (errored second run)\n";
     }
