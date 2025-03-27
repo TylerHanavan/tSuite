04be5d73e6ab6dc95622a2ffb31fcf18e003474a
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 07:28:45 -0400

    test missing setting balue

--- .tsuite/tests/RepoSettingAPITest.php
+++ .tsuite/tests/RepoSettingAPITest.php
@@ -11,6 +11,11 @@
         $response = test_curl($properties['endpoint_url'] . "/$uri", [], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
         assertEquals($response['response'], '{"status":"failed","error":"Empty payload"}', "$uri did not trigger error for empty payload");
+
+        // Test missing setting_value
+        $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_name' => 'test'], true);
+        assertEquals(400, $response['http_code'], "$uri http code mismatch");
+        assertEquals($response['response'], '{"status":"failed","error":"Missing setting_value"}', "$uri did not trigger error for missing setting_value");
     }
 
 ?>
