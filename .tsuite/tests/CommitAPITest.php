90b0eb010d50d361d45c4fd6e6228fcdd62523be
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 06:50:15 -0400

    make function for testing missing field

--- .tsuite/tests/CommitAPITest.php
+++ .tsuite/tests/CommitAPITest.php
@@ -24,7 +24,7 @@
         assertEquals($response['response'], '{"status":"failed","error":"Resources null or missing from payload"}', "$uri did not trigger error for empty payload");
 
         $entities = [];
-        
+
         $entity1['repo_id'] = 1;
 
         $entities[] = $entity1;
@@ -33,7 +33,24 @@
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
         assertEquals($response['response'], '{"status":"failed","error":"Resources null or missing from payload"}', "$uri did not trigger error for empty payload");
 
+        $entity1 = commit_testing_array_add_field_and_test_missing_field_response($properties, $uri, $entity1, 'hash', 'abcdefghi1234567', '{"status":"failed","error":"No hash provided for entity 0"}');
+
         echo "Concluded testing POST $uri (errored second run)\n";
+    }
+
+    function commit_testing_array_add_field_and_test_missing_field_response($properties, $uri, $array, $key, $value, $expected_response) {
+
+        $array[$key] = $value;
+
+        $entities = [];
+
+        $entities[] = $array;
+
+        $response = test_curl($properties['endpoint_url'] . "/$uri", $entities, true);
+        assertEquals(400, $response['http_code'], "$uri http code mismatch");
+        assertEquals($response['response'], $expected_response, "$uri did not trigger error for missing $key");
+
+        return $array;
     }
 
 ?>
