54138a472f18c7d1d811f85db2d19a706897a3f9
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 06:41:40 -0400

    commit api testing

---
+++ .tsuite/tests/CommitAPITest.php
@@ -0,0 +1,21 @@
+<?php
+
+    /** TODO:
+     * Test null payload
+     * Test string payload
+     */
+
+    function test_repo_post_1($properties) {
+        $uri = "/api/v1/commit";
+
+        $entities = array();
+
+        // Test missing payload
+        $response = test_curl($properties['endpoint_url'] . "/$uri", $entities, true);
+        assertEquals(400, $response['http_code'], "$uri http code mismatch");
+        assertEquals($response['response'], '{"status":"failed","error":"Resources array exists but is empty"}', "$uri did not trigger error for empty payload");
+
+        echo "Concluded testing POST $uri (errored second run)\n";
+    }
+
+?>
