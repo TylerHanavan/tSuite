660cd45a9cc381c60677a64e6361ce13c003e978
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 07:30:39 -0400

    switch expected and actual

--- .tsuite/tests/RepoSettingAPITest.php
+++ .tsuite/tests/RepoSettingAPITest.php
@@ -10,22 +10,22 @@
         // Test missing payload
         $response = test_curl($properties['endpoint_url'] . "/$uri", [], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals($response['response'], '{"status":"failed","error":"Empty payload"}', "$uri did not trigger error for empty payload");
+        assertEquals('{"status":"failed","error":"Empty payload"}', $response['response'], "$uri did not trigger error for empty payload");
 
         // Test missing setting_value
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_name' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals($response['response'], '{"status":"failed","error":"Missing setting_value"}', "$uri did not trigger error for missing setting_value");
+        assertEquals('{"status":"failed","error":"Missing setting_value"}', $response['response'], "$uri did not trigger error for missing setting_value");
 
         // Test missing setting_name
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_value' => 'test'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals($response['response'], '{"status":"failed","error":"Missing setting_name"}', "$uri did not trigger error for missing setting_name");
+        assertEquals('{"status":"failed","error":"Missing setting_name"}', $response['response'], "$uri did not trigger error for missing setting_name");
 
         // Test missing repo_id
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_value' => 'test', 'setting_name' => 'test1'], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
-        assertEquals($response['response'], '{"status":"failed","error":"Missing repo_id"}', "$uri did not trigger error for missing repo_id");
+        assertEquals('{"status":"failed","error":"Missing repo_id"}', $response['response'], "$uri did not trigger error for missing repo_id");
     }
 
 ?>
