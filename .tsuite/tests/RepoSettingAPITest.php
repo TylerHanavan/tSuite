d98b7d48dc266f8907bde342ad206844e0644994
Tyler Hanavan <TylerHanavan@gmail.com>
2025-03-27 07:52:33 -0400

    test additional characters

--- .tsuite/tests/RepoSettingAPITest.php
+++ .tsuite/tests/RepoSettingAPITest.php
@@ -34,7 +34,6 @@
         
         echo "Concluded testing POST $uri (errored first run)\n";
 
-        // Test bad repo_id (not exists in repo table)
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_value' => 'test', 'setting_name' => 'test1', 'repo_id' => 2], true);
         assertEquals(200, $response['http_code'], "$uri http code mismatch");
         assertEquals('[{"id":1,"repo_id":2,"name":"test1","value":"test"}]', $response['response'], "$uri did not get inserted");
@@ -45,7 +44,6 @@
     function test_repo_setting_get_1($properties) {
         $uri = '/api/v1/repo_setting';
 
-        // Test bad repo_id (not exists in repo table)
         $response = test_curl($properties['endpoint_url'] . "/$uri", [], false);
         assertEquals(200, $response['http_code'], "$uri http code mismatch");
         assertEquals('[{"id":1,"repo_id":2,"name":"test1","value":"test"}]', $response['response'], "$uri did not return the right entity");
@@ -56,15 +54,19 @@
     function test_repo_setting_post_2($properties) {
         $uri = '/api/v1/repo_setting';
 
-        // Test bad repo_id (not exists in repo table)
         $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_value' => 'test', 'setting_name' => 'test1', 'repo_id' => 2], true);
         assertEquals(400, $response['http_code'], "$uri http code mismatch");
         assertEquals('{"status":"failed","error":"repo_setting already exists for repo"}', $response['response'], "$uri did not trigger error for repo_setting already exists");
         
         echo "Concluded testing POST $uri (errored third run)\n";
 
+        $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_value' => 'jest', 'setting_name' => 'test2', 'repo_id' => 2], true);
+        assertEquals(200, $response['http_code'], "$uri http code mismatch");
+        assertEquals('[{"id":2,"repo_id":2,"name":"test2","value":"jest"}]', $response['response'], "$uri did not get inserted");
 
-
+        $response = test_curl($properties['endpoint_url'] . "/$uri", ['setting_value' => 'TENten12!@#$%^&*()-_+={[}];:\'",<.>/?\\|~`1234567890', 'setting_name' => 'Alpha_test-3', 'repo_id' => 1], true);
+        assertEquals(200, $response['http_code'], "$uri http code mismatch");
+        assertEquals('[{"id":3,"repo_id":1,"name":"test2Alpha_test-3","value":"TENten12!@#$%^&*()-_+={[}];:\'",<.>/?\\|~`1234567890"}]', $response['response'], "$uri did not get inserted");
 
         echo "Concluded testing POST $uri (successful fourth run)\n";
     }
